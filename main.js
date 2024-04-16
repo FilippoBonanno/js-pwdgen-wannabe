fee159f7bc6cadcf96608a5623c8c7dcbf50e4c0
FilippoBonanno <fbonanno15@gmail.com>
2024-04-16 16:56:45 +0200

    generate password and print

---
+++ main.js
@@ -0,0 +1,11 @@
+const number = "#24";
+
+let user = prompt ();
+
+let surname = prompt ();
+
+let color = prompt ();
+
+let password = user + surname + color + number
+
+document.getElementById("titolo").innerHTML = password 
